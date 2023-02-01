f42be364d3f5df56890040301851f613243b2698
Ajwad <aibnswalehin@gmail.com>
2023-02-01 22:13:56 +0000

    Pinpoints Anime Title
    Managed to print the title of the first anime in the season, also started work on emails

---
+++ main.py
@@ -0,0 +1,29 @@
+from jikanpy import Jikan
+import smtplib
+import emails
+from email.mime.text import MIMEText
+
+jikan = Jikan()
+seasonal_anime = jikan.seasons(2023, 'winter')
+print(seasonal_anime["data"][0]["title"])  # GIVES TITLE OF ANIME
+
+# server = smtplib.SMTP('smtp.gmail.com', 587)
+# server.starttls()
+
+# server.login(emails.sender_email, emails.sender_password)
+
+# anime_data = ''
+# for anime in seasonal_anime['anime']:
+#     anime_data += anime['title'] + '\n'
+
+# msg = MIMEText(anime_data)
+# msg['Subject'] = 'Seasonal Anime Data'
+# msg['From'] = emails.sender_email
+# msg['To'] = emails.recipient_email
+
+# server = smtplib.SMTP('smtp.gmail.com', 587)
+# server.ehlo()
+# server.starttls()
+# server.login(emails.sender_email, emails.sender_password)
+# server.send_message(msg)
+# server.quit()
