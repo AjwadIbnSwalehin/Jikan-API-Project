ed63fe8d10338ea574f59d6f8a9bc2a1d7f68154
Ajwad <aibnswalehin@gmail.com>
2023-02-02 22:15:55 +0000

    Find Each Anime Title and Email
    All the anime titles in the season are sent to the correct recipient email address from the correct sender email

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ from email.mime.text import MIMEText
 
 jikan = Jikan()
 seasonal_anime = jikan.seasons(2023, 'winter')
-print(seasonal_anime["data"][0]["title"])  # GIVES TITLE OF ANIME
 
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.starttls()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
 
-# server.login(emails.sender_email, emails.sender_password)
+server.login(emails.sender_email, emails.sender_password)
 
-# anime_data = ''
-# for anime in seasonal_anime['anime']:
-#     anime_data += anime['title'] + '\n'
+anime_data = ''
+for anime in seasonal_anime["data"]:
+    anime_data += anime['title'] + '\n'
 
-# msg = MIMEText(anime_data)
-# msg['Subject'] = 'Seasonal Anime Data'
-# msg['From'] = emails.sender_email
-# msg['To'] = emails.recipient_email
+msg = MIMEText(anime_data)
+msg['Subject'] = 'Seasonal Anime Data'
+msg['From'] = emails.sender_email
+msg['To'] = emails.recipient_email
 
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.ehlo()
-# server.starttls()
-# server.login(emails.sender_email, emails.sender_password)
-# server.send_message(msg)
-# server.quit()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(emails.sender_email, emails.sender_password)
+server.send_message(msg)
+server.quit()
