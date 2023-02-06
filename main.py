994d3cd06f151e541469b75173cafcff453aa813
Ajwad <aibnswalehin@gmail.com>
2023-02-06 22:14:18 +0000

    Sent email
    The program now sends an email with all the seasonal anime, as well as the image for the anime with it

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ from jikanpy import Jikan
 import smtplib
 import emails
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 jikan = Jikan()
 seasonal_anime = jikan.seasons(2023, 'winter')
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = smtplib.SMTP('smtp.gmail.com', 25)
 server.starttls()
-
 server.login(emails.sender_email, emails.sender_password)
 
-anime_data = ''
-for anime in seasonal_anime["data"]:
-    anime_data += anime['title'] + '\n'
-
-msg = MIMEText(anime_data)
+msg = MIMEMultipart()
 msg['Subject'] = 'Seasonal Anime Data'
 msg['From'] = emails.sender_email
 msg['To'] = emails.recipient_email
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
+text_template = "<h2>{}</h2>"
+
+anime_data = "<html><head></head><body>"
+for anime in seasonal_anime["data"]:
+    text = text_template.format(anime["title"])
+    image_url = anime["images"]["jpg"]["large_image_url"]
+    synopsis = anime["synopsis"]
+    anime_data += "<p>{}<br><img src='{}'></p>".format(text, image_url)
+
+anime_data += "</body></html>"
+
+text = MIMEText(anime_data, 'html')
+msg.attach(text)
+
 server.ehlo()
-server.starttls()
 server.login(emails.sender_email, emails.sender_password)
 server.send_message(msg)
 server.quit()
